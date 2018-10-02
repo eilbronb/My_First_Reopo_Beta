60ed0536c220a163f9505c3e55e800f485eb3281
Eilbron <ilbron@yahoo.com>
2018-10-01 21:57:01 -0700

    playing with lists

---
+++ lists.py
@@ -0,0 +1,31 @@
+my_list = [1,2,3,]
+print(my_list)
+print(my_list[0])
+
+#Appending 
+
+my_list.append(4)
+print(my_list)
+
+my_list.append('five')
+print(my_list)
+
+#Deleting 
+
+del my_list[3]
+print(my_list)
+
+#Nested Lists
+
+nested_list = []
+nested_list.append(123)
+nested_list.append(22)
+nested_list.append('ntp')
+nested_list.append('ssh')
+print(nested_list)
+
+my_list.append(nested_list)
+print(my_list)
+print(my_list[3])
+print(my_list[4])
+print(my_list[4][0])
