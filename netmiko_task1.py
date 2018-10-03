a66716a4b7b8a0636201d3ca66590a7cd5abd0d3
Eilbron <ilbron@yahoo.com>
2018-10-02 22:08:55 -0700

    added more files

---
+++ netmiko_task1.py
@@ -0,0 +1,6 @@
+from netmiko import ConnectHandler
+
+cisco_gns3 = {'device_type':'cisco_ios',
+              'ip':'10.10.10.30',
+              'username':'admin',
+              'password':'admin'}
