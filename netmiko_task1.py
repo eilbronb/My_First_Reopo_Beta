5f2fc88fb700206a73735a5e2058aa1ec70b9ace
Eilbron <ilbron@yahoo.com>
2018-10-04 22:00:29 -0700

    added more lines to netmiko

--- netmiko_task1.py
+++ netmiko_task1.py
@@ -12,3 +12,6 @@ print(hostname[:-1])
 
 interface_name = connection.send_command('show run int eth0/0')
 print(interface_name)
+
+interface_description = connection.send_command('show run int eth0/0' | i ^ des)
+print(interface_description)
