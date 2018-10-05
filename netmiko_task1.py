1e64f1f8a6c1e484eb5c0517d0cc911904eb13c8
Eilbron <ilbron@yahoo.com>
2018-10-04 22:07:10 -0700

    added more lines to netmiko

--- netmiko_task1.py
+++ netmiko_task1.py
@@ -10,10 +10,16 @@ connection = ConnectHandler(**cisco_gns3)
 hostname = connection.find_prompt()
 print(hostname[:-1])
 
-interface_name = connection.send_command('show run int eth0/0')
+interface_name = connection.send_command('show run int eth0/0 | i ^ interface')
 print(interface_name)
 
 interface_description = connection.send_command('show run int eth0/0 | i ^ des')
 if not interface_description:
     interface_description = 'N/A'
 print(interface_description)
+
+Config1 =['interface loopback0', 'ip address 100.100.100.100 255.255.255.255']
+
+connection.config_mode()
+create_loopback = connection.send_config_set(Config1)
+print(create_loopback)
