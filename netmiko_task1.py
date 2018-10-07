a9903a45d654a5428d5400314014a4f9373e513a
Eilbron <ilbron@yahoo.com>
2018-10-06 21:09:42 -0700

    added more lines to netmiko_task1

--- netmiko_task1.py
+++ netmiko_task1.py
@@ -31,15 +31,31 @@ else:
     print('It looks like something went wrong, let\'s get out before we break something!')
 print(verification)
 
-print('Moving on to Ethernet0/1')
-Config2 =['interface eth0/1','no switchport',
-         'des This goes to R1', 'ip address 10.10.30.30 255.255.255.0',
-         'no shut', 'exit']
+print('Moving on to Eth0/0, Eth0/1, Eth0/2 ')
+Config2 =['interface eth0/1','switchport',
+         'des To R1', 'switchport mode access' 'switchport access vlan 10',
+         'no shut', 'interface eth0/0','switchport',
+         'des To IOU-L3', 'switchport mode access' 'switchport access vlan 10',
+         'no shut', 'interface eth0/2','switchport',
+         'des To IOU2', 'switchport mode access' 'switchport access vlan 10',
+         'exit']
 connection.send_config_set(Config2)
 
 output0 = connection.send_command('show ip int eth0/1 | i Ethernet0/1')
 if output0.count('up') == 2:
     print('It Looks like Ethernet0/1 is "up/up", Way to go!')
+else:
+    print('It looks like Something went wrong, let\'s exit before we break something')
+    sys.exit()
+output1 = connection.send_command('show ip int eth0/0 | i Ethernet0/0')
+if output0.count('up') == 2:
+    print('It Looks like Ethernet0/0 is "up/up", Way to go!')
+else:
+    print('It looks like Something went wrong, let\'s exit before we break something')
+    sys.exit()
+output2 = connection.send_command('show ip int eth0/2 | i Ethernet0/2')
+if output0.count('up') == 2:
+    print('It Looks like Ethernet0/2 is "up/up", Way to go!')
 else:
     print('It looks like Something went wrong, let\'s exit before we break something')
     sys.exit()
