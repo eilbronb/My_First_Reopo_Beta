2df28822dfddbf5eae142333b705b727566db5ed
root <root@localhost.localdomain>
2018-10-21 18:37:19 -0700

    add

--- restconf-nxos.py
+++ restconf-nxos.py
@@ -8,8 +8,8 @@ url = 'https://10.10.10.25/restconf'
 https_header = {'content-type':'application/yang-data+json', 'accept':'application/yang-data+json'}
 
 resp = requests.get(f'{url}/data/Cisco-NX-OS-device:System/bgp-items/inst-items/dom-items/Dom-list?content=config', headers=https_header,  auth=('admin', 'password123'), verify=False)
-json_resp = resp.json()
+#json_resp = resp.json()
 
 print(resp.status_code)
 print(resp.text)
-print(json_resp)
+#print(json_resp)
