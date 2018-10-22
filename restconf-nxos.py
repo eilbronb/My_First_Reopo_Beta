074b8df693616f575d089b7b989a32a3d10616f6
Eilbron <ilbron@yahoo.com>
2018-10-21 18:16:08 -0700

    added restconf-nxos.py file

---
+++ restconf-nxos.py
@@ -0,0 +1,15 @@
+import requests
+import json
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+url = 'https://10.10.10.25/restconf'
+https_header = {'content-type':'application/yang-data+json', 'accept':'application/yang-data+json'}
+
+resp = requests.get(url, headers=https_header,  auth=('admin', 'password123'), verify=False)
+json_resp = resp.json()
+
+print(resp.status_code)
+print(resp.text)
+print(json_resp)
