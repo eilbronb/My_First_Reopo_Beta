d703b11111dc85abda05b0c621f8c52f85c79d82
Eilbron <ilbron@yahoo.com>
2018-10-21 18:20:45 -0700

    added restconf-nxos.py file

--- restconf-nxos.py
+++ restconf-nxos.py
@@ -7,7 +7,7 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 url = 'https://10.10.10.25/restconf'
 https_header = {'content-type':'application/yang-data+json', 'accept':'application/yang-data+json'}
 
-resp = requests.get(url, headers=https_header,  auth=('admin', 'password123'), verify=False)
+resp = requests.get(f'{url}/data/Cisco-NX-OS-device:System/bgp-items/inst-items/dom-items/Dom-list?content=config', headers=https_header,  auth=('admin', 'password123'), verify=False)
 json_resp = resp.json()
 
 print(resp.status_code)
