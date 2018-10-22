8bff4faa0dd886b490d14b86a2d3310cbc3ba466
root <root@localhost.localdomain>
2018-10-21 18:21:48 -0700

    local changes on vm

--- restconf-nxos.py
+++ restconf-nxos.py
@@ -8,8 +8,8 @@ url = 'https://10.10.10.25/restconf'
 https_header = {'content-type':'application/yang-data+json', 'accept':'application/yang-data+json'}
 
 resp = requests.get(url, headers=https_header,  auth=('admin', 'password123'), verify=False)
-json_resp = resp.json()
+#json_resp = resp.json()
 
 print(resp.status_code)
 print(resp.text)
-print(json_resp)
+#print(json_resp)
