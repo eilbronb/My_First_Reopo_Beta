4085559572f488e5b354826ade7abfb0e515a367
Eilbron <ilbron@yahoo.com>
2018-10-21 17:55:48 -0700

    changed http back to https

--- first-nxos-api.py
+++ first-nxos-api.py
@@ -8,6 +8,8 @@ url = 'https://10.10.10.25/ins'
 https_header = {'content-type':'application/json-rpc'}
 
 reqList = []
+showList = []
+
 reqItem1 = {
     "jsonrpc":"2.0",
     "method":"cli",
@@ -38,17 +40,27 @@ showItem1 = {
     "params": {"cmd":"show ip interface brief", "version": 1},
     "id": 1
 }
+showList.append(showItem1)
+
+showItem2 = {
+    "jsonrpc":"2.0",
+    "method":"cli",
+    "params": {"cmd":"show version" , "version": 1},
+    "id": 1
+}
+showList.append(showItem2)
+
 
 https_payload = json.dumps(reqList)
 resp = requests.post(url, data=https_payload, headers=https_header, auth=('admin' , 'password123'), verify=False)
 json_resp = resp.json()
 
 
-
 #Creating another call for the show command
-http_resp = json.dumps(showItem1)
+http_resp = json.dumps(showList)
 resp1 = requests.post(url, data=http_resp, headers=https_header ,  auth=('admin' , 'password123'), verify=False)
 json_resp1 = resp1.json()
+
 
 #creating a map to get to Data
 print(json_resp1["result"]["body"]["TABLE_intf"])
